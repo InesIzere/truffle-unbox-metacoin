d3e4e6248306ce3e771c8495efc974222f6dcdee
InesIzere <izere.ines@gmail.com>
2021-04-13 08:21:27 -0400

    first

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+  const HDWalletProvider = require("@truffle/hdwallet-provider");
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
